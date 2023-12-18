5f53b27cb8abc53aef50b8245bf660cfe842dc69
Facundo <castrofacundo19@gmail.com>
2023-12-18 17:12:26 -0300

    avance re paja

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-// Randmonly returns Rock, Paper or Scissors
-const getComputerChoice = () => {
+let playerChoice
+const piedraButton = document.getElementById('piedra')
+const papelButton = document.getElementById('papel')
+const tijeraButton = document.getElementById('tijera')
 
+// Randmonly returns Piedra, Papel o Tijera
+const getComputerChoice = () => {
+    const computerChoice = Math.floor(Math.random() * 3)
+    switch(computerChoice) {
+        case 0:
+            return "Piedra"
+        case 1:
+            return "Papel"
+        case 2:
+            return "Tijera"
+    }
 }
+
+
+// Takes both inputs and returns the winner
+const playRound = (playerChoice, computerChoice) => {
+    if (playerChoice == computerChoice) {
+        return "Empate"
+    } else if (playerChoice) {
+
+    }
+}
+
