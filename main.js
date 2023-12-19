1aac51c60b0c1f871eadf30c5a7cd21b0a831ac0
Facundo <castrofacundo19@gmail.com>
2023-12-18 21:31:38 -0300

    avanzando de a poco

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 let playerChoice
+let computerChoice
 const piedraButton = document.getElementById('piedra')
 const papelButton = document.getElementById('papel')
 const tijeraButton = document.getElementById('tijera')
+let resultTextbox = document.getElementById('result')
+let computerChoiceBox = document.getElementById('computerChoice')
 
 // Randmonly returns Piedra, Papel o Tijera
 const getComputerChoice = () => {
-    const computerChoice = Math.floor(Math.random() * 3)
+    computerChoice = Math.floor(Math.random() * 3)
     switch(computerChoice) {
         case 0:
-            return "Piedra"
+            computerChoice = 'piedra'
+            return computerChoice
         case 1:
-            return "Papel"
+            computerChoice = 'papel'
+            return computerChoice
         case 2:
-            return "Tijera"
+            computerChoice = 'tijera'
+            return computerChoice
     }
 }
 
 
 // Takes both inputs and returns the winner
-const playRound = (playerChoice, computerChoice) => {
+const checkWinner = (playerChoice, computerChoice) => {
     if (playerChoice == computerChoice) {
         return "Empate"
-    } else if (playerChoice) {
-
+    } else if ((playerChoice == 'piedra' && computerChoice == 'tijera') ||
+               (playerChoice == 'papel' && computerChoice == 'piedra') ||
+               (playerChoice == 'tijera' && computerChoice == 'papel')) {
+        return 'Gana Player'
+    } else {
+        return 'Gana Computer'
     }
 }
 
+// When PPT button pressed
+piedraButton.addEventListener('click', function() {
+    playerChoice = 'piedra'
+    resultTextbox.textContent = checkWinner(playerChoice, getComputerChoice())
+    computerChoiceBox.textContent = computerChoice
+})
